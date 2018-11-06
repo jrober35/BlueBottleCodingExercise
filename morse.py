4260ca06c7b4d0821777699f4b07e3716b259d2e
John Robertson <johnrobertson25@aim.com>
2018-11-06 00:13:50 -0800

    Second Commit

--- morse.py
+++ morse.py
@@ -3,10 +3,12 @@ import csv
 import requests
 import datetime
 
+# Create table, download temperature data, and load into Postgresql database
 def loadDatabase():
 	conn = psycopg2.connect("host=localhost dbname=postgres user=postgres")
 	cur = conn.cursor()
 
+	# Create a table in Postgres database containing appropriate headers
 	cur.execute("""
 	CREATE TABLE sales(
 		local_created_at timestamp,
@@ -18,6 +20,7 @@ def loadDatabase():
 
 	url = 'https://api.darksky.net/forecast/4e0f00e05cb7e94eb00423400ffe686c/37.831106,-122.254110,'
 
+	# Open the data file, read entries, and insert values into database columns
 	with open('morse.csv', 'r') as f:
 		reader = csv.reader(f)
 		next(reader)
@@ -36,12 +39,12 @@ def loadDatabase():
 				"INSERT INTO sales VALUES (%s, %s, %s, %s)", row
 			)
 			ctr += 1
-			if ctr > 999:
+			if ctr > 99: # API source had a daily limit on requests, so cut off at 100
 				break
 	conn.commit()
 
 def main():
-	loadDatabase(); #Create table, download temperature data, and load into Postgresql database
+	loadDatabase();
 
 if __name__ == "__main__":
 	main()
